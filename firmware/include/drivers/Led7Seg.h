a2bc0df781e76db3bed4c4866578933cde45ef20
THANH <le627959@gmail.com>
2026-06-13 19:30:49 +0700

    Add declaration function in Led7Seg.h

--- firmware/include/drivers/Led7Seg.h
+++ firmware/include/drivers/Led7Seg.h
@@ -1,8 +1,20 @@
-
-// The function that Led_out the number , using state to choose which led out,
+/* *
+ * Transmit data to Led module depends on state variable
+ * @param:
+ * **number**: data to transmit
+ * **state** : choose which module led is out
+ * @return:
+ * 1 = transmit completed
+ * 0 = transmit uncompleted
+ * */
 char Led_out(unsigned int number, char state);
 // State = 0 : Led num 1
 // State = 1 : Led num 2
 // State = 2 : Led result 3
 
+/* *
+ * Init for Led 7 segment module
+ * @setup: turn on display, wake up IC from sleep mode
+ * clear dump data, start with clear data
+ * */
 void Leg7Seg_init(void);
