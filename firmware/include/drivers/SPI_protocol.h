9a0dc1db8e08b56b568671e31088c820824fbed0
THANH <le627959@gmail.com>
2026-06-13 22:02:03 +0700

    Add declaration for SPI_protocol.h

--- firmware/include/drivers/SPI_protocol.h
+++ firmware/include/drivers/SPI_protocol.h
@@ -1,5 +1,8 @@
-// The function that initialization for SPI
+/* *
+ * The function that initialization for SPI
+ * @setup : mode 0, set output for SS CLK MOSI
+ * */
 void SPI_MasterInit(void);
-// Transmit data function
+
+// Transmit data with SPI protocol
 void SPI_MasterTransmit(char cData);
-// Receive data function
